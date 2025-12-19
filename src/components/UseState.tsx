518d8d91ded7a6f1bc041efe4037faec891c4418
Oscar Munoz Bernales <munoz.bernales.oscar@gmail.com>
2025-12-19 18:00:09 -0300

    ✅ UPD: Estados imperativos con useState

--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -5,11 +5,30 @@ const SECURITY_CODE = "paradigma";
 
 export const UseState = (props: PropsUseStateInterface): React.ReactElement => {
 
-    const [{ error, loading, value }, setState] = React.useState<StateUseStateInterface>({
+    const INITIAL_STATE: StateUseStateInterface = {
         error: false,
         loading: false,
         value: "",
-    });
+        confirmed: false,
+        deleted: false,
+    };
+
+    const [{ error, loading, value, confirmed, deleted }, setState] = React.useState<StateUseStateInterface>(INITIAL_STATE);
+
+    const changeInputValue = (newValue: string) => {
+        setState((prev: StateUseStateInterface) => ({
+            ...prev,
+            error: false,
+            value: newValue,
+        }));
+    }
+
+    const searchValue = () => {
+        setState((prev: StateUseStateInterface) => ({
+            ...prev,
+            loading: !prev.loading,
+        }));
+    }
 
     React.useEffect(() => {
         if (!!loading) {
@@ -18,44 +37,66 @@ export const UseState = (props: PropsUseStateInterface): React.ReactElement => {
                     ...prev,
                     loading: false,
                     error: prev.value !== SECURITY_CODE,
+                    confirmed: prev.value === SECURITY_CODE,
                 }));
             }, 3000);
         }
     }, [ loading ]); // useEffect se vuelve a ejecutar cuando cambia "loading"
 
+    if ( !confirmed && !deleted ) {
     return (
         <div>
             <h2>Eliminar {props.name}</h2>
 
             <p>Por favor, escribe el código de seguridad.</p>
 
-            {
-                error && (<p>Error: el código es incorrecto</p>)
-            }
+            { error && (<p>Error: el código es incorrecto</p>) }
 
-            {
-                loading && (<p>Cargando...</p>)
-            }
+            { loading && (<p>Cargando...</p>) }
 
             <input 
                 placeholder="Código de seguridad" 
                 value={value}
-                onChange={(event) => {
-                    setState((prev: StateUseStateInterface) => ({
-                        ...prev,
-                        error: false,
-                        value: event.target.value,
-                    }));
-                }}
+                onChange={(event) => changeInputValue(event.target.value)}
             />
             <button
-                onClick={() =>
-                    setState((prev: StateUseStateInterface) => ({
-                        ...prev,
-                        loading: !prev.loading
-                    }))
-                }
+                onClick={() => searchValue()}
             >Comprobar</button>
         </div>
     );
+    } else if ( confirmed && !deleted ) {
+        return (
+            <div>
+                <h2>Eliminar {props.name}</h2>
+
+                <p>¿Estás seguro de que quieres eliminarlo?</p>
+
+                <button
+                    onClick={() => setState((prev: StateUseStateInterface) => ({
+                        ...prev,
+                        deleted: true,
+                    }))}
+                >Sí, eliminar</button>
+                <button
+                    onClick={() => setState((prev: StateUseStateInterface) => ({
+                        ...prev,
+                        confirmed: false,
+                        value: "",
+                    }))}
+                >No, me arrepentí</button>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h2>Eliminar {props.name}</h2>
+
+                <p>Eliminado con éxito.</p>
+
+                <button
+                    onClick={() => setState({...INITIAL_STATE})}
+                >Volver</button>
+            </div>
+        );
+    }
 };
--- src/interfaces/use-state.interface.ts
+++ src/interfaces/use-state.interface.ts
@@ -6,4 +6,9 @@ export interface StateUseStateInterface {
     error: boolean;
     loading: boolean;
     value: string;
+    confirmed: boolean;
+    deleted: boolean;
 }
+
+//Como exportar las llaves de una interfaz
+export type StateUseStateKeys = keyof StateUseStateInterface;
